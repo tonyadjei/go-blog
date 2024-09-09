5e2abb5f83e903deb727a91f465a1ee3c1a59dcf
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2024-09-09 19:00:00 +0200

    create custom middleware for logging request data

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,10 +6,13 @@ import (
 	"tonyadjei/go-blog.git/pkg/handlers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler{
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(LogRequestInfo)
 	mux.Get("/", handlers.Repo.HomeHandler)
 	mux.Get("/about", handlers.Repo.AboutHandler)
 	return mux
